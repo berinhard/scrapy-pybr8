721751949d566983239716420c5c1352f95c2dd3
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-11-22 11:39:54 -0200

    Esqueleto inicial do projeto

---
+++ pybr8talks/pybr8talks/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for pybr8talks project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'pybr8talks'
+
+SPIDER_MODULES = ['pybr8talks.spiders']
+NEWSPIDER_MODULE = 'pybr8talks.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'pybr8talks (+http://www.yourdomain.com)'
