e3a73901c10f7e478de9e499c7fa0e687f94bf52
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-11-22 12:14:58 -0200

    Retrieve talks urls and log them

--- pybr8talks/pybr8talks/spiders/talks.py
+++ pybr8talks/pybr8talks/spiders/talks.py
@@ -1,4 +1,7 @@
+from scrapy import log
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
 
 class TalksSpider(BaseSpider):
     name = 'talks'
@@ -6,4 +9,14 @@ class TalksSpider(BaseSpider):
     start_urls = ['http://2012.pythonbrasil.org.br/schedule']
 
     def parse(self, response):
-        pass
+        talks_urls = self.get_talks_urls(response)
+        for talk_url in talks_urls:
+            self.log('Retrieved URL: %s' % talk_url, level=log.DEBUG)
+        return None
+
+    def get_talks_urls(self, response):
+        hxs = HtmlXPathSelector(response)
+        for path in hxs.select('//td/a/@href').extract():
+            protocol = 'http://'
+            domain = self.allowed_domains[0]
+            yield protocol + domain + path
