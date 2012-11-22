4c9948e17bfcce66a16effedf115a738c4531e0c
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-11-22 11:51:37 -0200

    Por caráter instrutivo, modifica spider para herdar de BaseSpider

--- pybr8talks/pybr8talks/spiders/talks.py
+++ pybr8talks/pybr8talks/spiders/talks.py
@@ -1,21 +1,9 @@
-from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from pybr8talks.items import Pybr8TalksItem
+from scrapy.spider import BaseSpider
 
-class TalksSpider(CrawlSpider):
+class TalksSpider(BaseSpider):
     name = 'talks'
     allowed_domains = ['2012.pythonbrasil.org.br']
-    start_urls = ['http://www.2012.pythonbrasil.org.br/']
+    start_urls = ['http://2012.pythonbrasil.org.br/schedule']
 
-    rules = (
-        Rule(SgmlLinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
-        hxs = HtmlXPathSelector(response)
-        i = Pybr8TalksItem()
-        #i['domain_id'] = hxs.select('//input[@id="sid"]/@value').extract()
-        #i['name'] = hxs.select('//div[@id="name"]').extract()
-        #i['description'] = hxs.select('//div[@id="description"]').extract()
-        return i
+    def parse(self, response):
+        pass
