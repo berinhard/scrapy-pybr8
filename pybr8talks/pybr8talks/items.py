00c4ad5d9a2f002675398fe108365a8be4bc4fc9
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-11-22 19:29:15 -0200

    Clean the output by serializing it correctly

--- pybr8talks/pybr8talks/items.py
+++ pybr8talks/pybr8talks/items.py
@@ -1,7 +1,12 @@
 from scrapy.item import Item, Field
 
+def strip_and_join(values):
+    value = values[0].strip()
+    joined = u' '.join(value.split('\n'))
+    return joined.replace('\r', '')
+
 class Pybr8TalksItem(Item):
     title = Field()
-    description = Field()
-    author_name = Field()
-    author_profile = Field()
+    description = Field(serializer=strip_and_join)
+    author_name = Field(serializer=strip_and_join)
+    author_profile = Field(serializer=strip_and_join)
